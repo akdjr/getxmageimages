cfa7a6b3ed2f2756f26314f34a975bf469c4b1a1
Akshay Dhalwala <akshay.dhalwala@gmail.com>
2017-10-02 16:02:30 -0700

    Basic error handling and usage.

--- getimages.js
+++ getimages.js
@@ -14,71 +14,86 @@ function processPage (set, url, fns, done) {
 		url: url,
 		json: true
 	}, function (error, response, body) {
-		body.data.forEach((card, index) => {
-			// check if it this card has alternate art by seeing if a card with a duplicate name exists
-			// scryfall's default sort is by name, so we just look before or after the current card
-			var alternate = false;
-			if (body.data[index-1]) {
-				if (card.name === body.data[index-1].name) {
-					alternate = true;
-				}
-			}
+		var stop = false;
 
-			if (body.data[index+1]) {
-				if (card.name === body.data[index+1].name) {
-					alternate = true;
+		if (error || (!response) || (response && response.statusCode !== 200)) {
+			console.log('An error occured while trying to query ScryFall');
+			console.log(error);
+			console.log(`${statusCode}: ${body}`);
+			stop = true;
+		} else {
+			body.data.forEach((card, index) => {
+				// check if it this card has alternate art by seeing if a card with a duplicate name exists
+				// scryfall's default sort is by name, so we just look before or after the current card
+				var alternate = false;
+				if (body.data[index-1]) {
+					if (card.name === body.data[index-1].name) {
+						alternate = true;
+					}
 				}
-			}
 
-			// check if this is a multi-face card
-			if (card.card_faces && card.card_faces.length > 0) {
-				// process each face as an individual card
-				card.card_faces.forEach((card_face) => {
+				if (body.data[index+1]) {
+					if (card.name === body.data[index+1].name) {
+						alternate = true;
+					}
+				}
+
+				// check if this is a multi-face card
+				if (card.card_faces && card.card_faces.length > 0) {
+					// process each face as an individual card
+					card.card_faces.forEach((card_face) => {
+						fns.push((next) => {
+							console.log(`Downloading image for '${card_face.name}'`);
+							if (!fs.existsSync(`./${set}`)) {
+								fs.mkdirSync(`./${set}`);
+							}
+							// download the image
+							// xmage requires the card collector number in the filename to indicate alternate art versions of a card
+							var stream = request(card_face.image_uris.normal)
+								.pipe(fs.createWriteStream(`./${set}/${card_face.name}${alternate ? `.${card.collector_number}` : ''}.full.jpg`))
+								.on('finish', () => {
+									setTimeout(() => {
+										return next();
+									}, delay);
+								});
+						});
+					});
+				} else {
 					fns.push((next) => {
-						console.log(`Downloading image for '${card_face.name}'`);
+						console.log(`Downloading image for '${card.name}'`);
 						if (!fs.existsSync(`./${set}`)) {
 							fs.mkdirSync(`./${set}`);
 						}
+
 						// download the image
 						// xmage requires the card collector number in the filename to indicate alternate art versions of a card
-						var stream = request(card_face.image_uris.normal)
-							.pipe(fs.createWriteStream(`./${set}/${card_face.name}${alternate ? `.${card.collector_number}` : ''}.full.jpg`))
+						var stream = request(card.image_uris.normal)
+							.pipe(fs.createWriteStream(`./${set}/${card.name}${alternate ? `.${card.collector_number}` : ''}.full.jpg`))
 							.on('finish', () => {
 								setTimeout(() => {
 									return next();
 								}, delay);
 							});
 					});
-				});
-			} else {
-				fns.push((next) => {
-					console.log(`Downloading image for '${card.name}'`);
-					if (!fs.existsSync(`./${set}`)) {
-						fs.mkdirSync(`./${set}`);
-					}
-
-					// download the image
-					// xmage requires the card collector number in the filename to indicate alternate art versions of a card
-					var stream = request(card.image_uris.normal)
-						.pipe(fs.createWriteStream(`./${set}/${card.name}${alternate ? `.${card.collector_number}` : ''}.full.jpg`))
-						.on('finish', () => {
-							setTimeout(() => {
-								return next();
-							}, delay);
-						});
-				});
-			}
-		});
+				}
+			});
+		}
 
 		setTimeout(() => {
-			if (body.has_more) {
-				process.nextTick(() => {
-					processPage(set, body.next_page, fns, done);
-				});
-			} else {
+			if (stop) {
 				process.nextTick(() => {
 					done();
 				});
+			} else {
+				if (body.has_more) {
+					process.nextTick(() => {
+						processPage(set, body.next_page, fns, done);
+					});
+				} else {
+					process.nextTick(() => {
+						done();
+					});
+				}
 			}
 		}, delay);
 	});
@@ -96,10 +111,23 @@ function downloadImages (set) {
 	});
 }
 
-var set = 'XLN';
-
-if (process.argv.length > 2) {
-	set = process.argv[3].toUpperCase();	
+function printUsage() {
+	console.log('Usage:');
+	console.log('./getimages-win.exe <set>');
+	console.log('./getimages-win.exe help');
 }
 
-downloadImages(set);
+if (process.argv.length > 2) {
+	var set = process.argv[3].toUpperCase();
+
+	if (set === 'HELP') {
+		printUsage();
+		process.exit(0);
+	} else {
+		downloadImages(set);
+	}
+} else {
+	printUsage();
+	process.exit(0);
+}
+
